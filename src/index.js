c81c529ced23eae153fa384a0575a42b73be9b8e
kaetaen <kaetaen@tutanota.com>
2022-03-10 17:10:44 -0300

    feat: add fetch API data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('start project')
+const axios = require('axios')
+
+async function getUniversityAPIData (country_name) {
+    const BASE_URL = "http://universities.hipolabs.com/search?country="
+
+    try {
+
+        if (!country_name) 
+            throw "Erro: País não informado"
+        
+        if (!countries.includes(country_name)) 
+            throw "Erro: País não listado"
+        
+        const { data } = await axios.get(BASE_URL + country_name)
+        return data
+
+    } catch (e) {
+       console.log(e)
+    }
+}
+
+
+async function run () {
+    const countries = [
+        "argentina",
+        "brazil",
+        "chile",
+        "colombia",
+        "paraguay",
+        "peru",
+        "suriname",
+        "uruguay"
+    ]
+
+    for (country in countries) {
+        const universityData = await getUniversityAPIData(country)
+    }
+}
+
+run()
